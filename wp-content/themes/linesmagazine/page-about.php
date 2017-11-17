22148335301fa2d6be47373ae23c0680143be88f
NERDYLIZARD <bouasavanhhop@gmail.com>
2017-11-17 11:05:54 +0530

    about page (roughly)

---
+++ wp-content/themes/linesmagazine/page-about.php
@@ -0,0 +1,111 @@
+<?php
+/**
+ * Template Name: About Page
+ */
+
+get_header(); ?>
+
+  <!-- Mission Statement -->
+  <section id="mission" class="image-feature-background overlay-dark">
+    <h1>Our Mission is to send a man to the Mar.</h1>
+<!--    <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores dignissimos inventore magnam nemo nulla placeat, tenetur ut voluptatem! Consequuntur deleniti dolorum et quibusdam sequi, voluptatibus? Debitis impedit molestias necessitatibus? Illo.</h1>-->
+    <!--<h1>ສະຖາປັດຕະຍະກຳພາຍນອກທີ່ບໍ່ໂດດເດັ່ນ</h1>-->
+  </section>
+
+  <section id="about">
+    <div class="container">
+      <h2>About us</h2>
+      <p>ສະຖາປັດຕະຍະກຳພາຍນອກທີ່ບໍ່ໂດດເດັ່ນ ເປັນອາຄານຊັ້ນດຽວ ມຸງຫລັງຄາຊົງຕ່ຳ ມີຮູບລັກສະນະລຽບງ່າຍຕາມຮູບແບບເຮືອນອາເມຣິກາ ຖືກສ້າງຂຶ້ນໃນສະໄໝກ່ອນປົດປ່ອຍ ໂດຍອອກແບບໃຫ້ເປັນທີ່ພັກຮອງຮັບໃຫ້ກັບຄອບຄົວນາຍທະຫານອາເມຣິກາ ໃນຄ້າຍທະຫານຫລັກ 6, ເຊິ່ງພາຍຫລັງທີ່ປະເທດຊາດໄດ້ຮັບການປະກາດເອກະລາດ ເຂດເຮືອນນີ້ຖືກປັບປ່ຽນໝູນໃຊ້ໃຫ້ກາຍມາເປັນບ່ອນປະຈຳການຂອງອະດີດປະທານໄກສອນພົມວິຫານ, ຜູ້ນຳທີ່ແສນເຄົາລົບຮັກຂອງປວງຊົນລາວ ແລະໄດ້ຖືກປັບປຸງຟື້ນຟູກັບຄືນມາໃໝ່ອີກຄັ້ງໃຫ້ເປັນອະນຸສອນສະຖານ ປະທານໄກສອນ ພົມວິຫານ ໃນວັນທີ 13 ທັນວາ 1994 ກົງກັບວັນຄ້າຍວັນເກີດຂອງທ່ານຄົບຮອບ 74 ປີ, ໂດຍໄດ້ມີການເກັບຮັກສາສະພາບຂອງອາຄານຕະຫຼອດເຖິງສິ່ງຂອງເຄື່ອງໃຊ້ສ່ວນຕົວຂອງທ່ານ ເຊິ່ງສິ່ງເຫລົ່ານີ້ເຮັດໃຫ້ຜູ້ເຂົ້າມາຢ້ຽມຊົມໄດ້ສຳຜັດກິ່ນອາຍ ທີ່ບອກໄດ້ເຖິງເລື່ອງລາວທີ່ເກີດຂຶ້ນ ໃນທຸກບາດກ້າວການດຳເນີນຊີວິດຂອງອະດີດທ່ານປະທານໄກສອນຢ່າງໃກ້ຊິດ ຄືກັບວ່າໄດ້ຫວນຍ້ອນອະດີດໄປເຫັນພາບຕອນທີ່ເພິ່ນຍັງມີຊີວິດຢູ່.</p>
+    </div>
+  </section>
+
+  <div class="separator"></div>
+
+  <section id="team" class="border-bottom">
+    <div class="container">
+      <h2>Our Team</h2>
+
+      <div class="row justify-content-start">
+
+        <div class="col-sm-6 col-md-4">
+          <div class="team-member">
+            <div class="image-wrapper image-round team-image"><img src="<?php echo get_template_directory_uri() . '/assets/images/picT1.png' ?>" alt=""></div>
+            <h3 class="name">Saychai Phanhthavong</h3>
+            <p class="position">Software Developer</p>
+            <p class="bio">ສະຖາປັດຕະຍະກຳພາຍນອກທີ່ບໍ່ໂດດເດັ່ນ ເປັນອາຄານຊັ້ນດຽວ ມຸງຫລັງຄາຊົງຕ່ຳ ມີຮູບລັກສະນະລຽບງ່າຍຕາມຮູບແບບເຮືອນອາເມຣິກາ ຖືກສ້າງຂຶ້ນໃນສະໄໝກ່ອນປົດປ່ອຍ ໂດຍອອກແບບໃຫ້ເປັນທີ່ພັກຮອງຮັບໃຫ້ກັບຄອບຄົວນາຍທະຫານອາເມຣິກາ ໃນຄ້າຍທະຫານຫລັກ 6, ເຊິ່ງພາຍຫລັງທີ່ປະເທດຊາດໄດ້ຮັບການປະກາດເອກະລາດ ເຂດເຮືອນນີ້ຖືກປັບປ່ຽນໝູນໃຊ້ໃຫ້ກາຍມາເປັນບ່ອນປະຈຳການຂອງອະດີດປະທານໄກສອນພົມວິຫານ.</p>
+            <div class="social-icons">
+              <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-phone-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-envelope-square" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="team-member">
+            <div class="image-wrapper image-round team-image"><img src="<?php echo get_template_directory_uri() . '/assets/images/picT2.png' ?>" alt=""></div>
+            <h3 class="name">Lorem Ipsum</h3>
+            <p class="position">Graphic Design</p>
+            <p class="bio">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis culpa eveniet id in magnam pariatur perferendis provident quis sapiente similique. Assumenda debitis dolor, dolorum eius excepturi porro sunt velit voluptas!</p>
+            <div class="social-icons">
+              <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-phone-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-envelope-square" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="team-member">
+            <div class="image-wrapper image-round team-image"><img src="<?php echo get_template_directory_uri() . '/assets/images/picT3.png' ?>" alt=""></div>
+            <h3 class="name">Saychai Phanhthavong</h3>
+            <p class="position">Editor</p>
+            <p class="bio">ສະຖາປັດຕະຍະກຳພາຍນອກທີ່ບໍ່ໂດດເດັ່ນ ເປັນອາຄານຊັ້ນດຽວ ມຸງຫລັງຄາຊົງຕ່ຳ ມີຮູບລັກສະນະລຽບງ່າຍຕາມຮູບແບບເຮືອນອາເມຣິກາ ຖືກສ້າງຂຶ້ນໃນສະໄໝກ່ອນປົດປ່ອຍ ໂດຍອອກແບບໃຫ້ເປັນທີ່ພັກຮອງຮັບໃຫ້ກັບຄອບຄົວນາຍທະຫານອາເມຣິກາ ໃນຄ້າຍທະຫານຫລັກ 6, ເຊິ່ງພາຍຫລັງທີ່ປະເທດຊາດໄດ້ຮັບການປະກາດເອກະລາດ ເຂດເຮືອນນີ້ຖືກປັບປ່ຽນໝູນໃຊ້ໃຫ້ກາຍມາເປັນບ່ອນປະຈຳການຂອງອະດີດປະທານໄກສອນພົມວິຫານ.</p>
+            <div class="social-icons">
+              <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-phone-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-envelope-square" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="team-member">
+            <div class="image-wrapper image-round team-image"><img src="<?php echo get_template_directory_uri() . '/assets/images/picT2.png' ?>" alt=""></div>
+            <h3 class="name">Saychai Phanhthavong</h3>
+            <p class="position">Photographer</p>
+            <p class="bio">ສະຖາປັດຕະຍະກຳພາຍນອກທີ່ບໍ່ໂດດເດັ່ນ ເປັນອາຄານຊັ້ນດຽວ ມຸງຫລັງຄາຊົງຕ່ຳ ມີຮູບລັກສະນະລຽບງ່າຍຕາມຮູບແບບເຮືອນອາເມຣິກາ ຖືກສ້າງຂຶ້ນໃນສະໄໝກ່ອນປົດປ່ອຍ ໂດຍອອກແບບໃຫ້ເປັນທີ່ພັກຮອງຮັບໃຫ້ກັບຄອບຄົວນາຍທະຫານອາເມຣິກາ ໃນຄ້າຍທະຫານຫລັກ 6, ເຊິ່ງພາຍຫລັງທີ່ປະເທດຊາດໄດ້ຮັບການປະກາດເອກະລາດ ເຂດເຮືອນນີ້ຖືກປັບປ່ຽນໝູນໃຊ້ໃຫ້ກາຍມາເປັນບ່ອນປະຈຳການຂອງອະດີດປະທານໄກສອນພົມວິຫານ.</p>
+            <div class="social-icons">
+              <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-phone-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-envelope-square" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-sm-6 col-md-4">
+          <div class="team-member">
+            <div class="image-wrapper image-round team-image"><img src="<?php echo get_template_directory_uri() . '/assets/images/picT1.png' ?>" alt=""></div>
+            <h3 class="name">Saychai Phanhthavong</h3>
+            <p class="position">Software Developer</p>
+            <p class="bio">ສະຖາປັດຕະຍະກຳພາຍນອກທີ່ບໍ່ໂດດເດັ່ນ ເປັນອາຄານຊັ້ນດຽວ ມຸງຫລັງຄາຊົງຕ່ຳ ມີຮູບລັກສະນະລຽບງ່າຍຕາມຮູບແບບເຮືອນອາເມຣິກາ ຖືກສ້າງຂຶ້ນໃນສະໄໝກ່ອນປົດປ່ອຍ ໂດຍອອກແບບໃຫ້ເປັນທີ່ພັກຮອງຮັບໃຫ້ກັບຄອບຄົວນາຍທະຫານອາເມຣິກາ ໃນຄ້າຍທະຫານຫລັກ 6, ເຊິ່ງພາຍຫລັງທີ່ປະເທດຊາດໄດ້ຮັບການປະກາດເອກະລາດ ເຂດເຮືອນນີ້ຖືກປັບປ່ຽນໝູນໃຊ້ໃຫ້ກາຍມາເປັນບ່ອນປະຈຳການຂອງອະດີດປະທານໄກສອນພົມວິຫານ.</p>
+            <div class="social-icons">
+              <a href="#"><i class="fa fa-facebook-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-linkedin-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-phone-square" aria-hidden="true"></i></a>
+              <a href="#"><i class="fa fa-envelope-square" aria-hidden="true"></i></a>
+            </div>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  </section>
+
+<?php
+get_footer();
